d38e265436094efb3dcde77a0ae4ce4970f1d21a
AndreaKaja <maedchenauge42@t-online.de>
2024-11-30 01:31:09 +0100

    json to py hinzugefügt

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import json
 
 
-print("hallo")
+#print("hallo")
+
+class Attribute:
+
+    def __init__(self, name, type, primary):
+        self.name = name
+        self.type = type
+        self.primary = primary
 
 
 class Entity:
@@ -11,16 +18,24 @@ class Entity:
         self.attributes = attributes
 
 
+
 class Relation:
     """Der Name ist der Name der Relation, entity_left das Objekt der einen Entität, right das der anderen"""
 
-    def __init__(self, name, entity_left, entity_right, cardi_left, card_right):
+    def __init__(self, name):
         self.name = name
-        self.entity_left = entity_left
-        self.entity_right = entity_right
-        self.cardi_left = cardi_left
-        self.card_right = card_right
+        self.entity_left = None
+        self.entity_right = None
+        self.cardi_left = None
+        self.cardi_right = None
 
+    def set_left(self, entity_left, cardi_left):
+        self.entity_left = entity_left
+        self.cardi_left = cardi_left
+
+    def set_right(self, entity_right, cardi_right):
+        self.entity_right = entity_right
+        self.cardi_right = cardi_right
 
 # die JSON-Daten zu SQL konvertieren
 def json_to_sql(entities, relations):
@@ -40,6 +55,34 @@ def json_to_sql(entities, relations):
 # aus dem SQL die Datenbank erstellen (also Anfragen an das DBMS senden, damit dieses die Datenbank erstellt)
 def sql_to_dbms():
     pass
+
+
+def json_to_py(json):
+    entities = []
+    relations = []
+    for entity in json["entities"]:
+        attributes = []
+        for attribute in entity["attributes"]:
+            attributes.append(Attribute(attribute["name"], attribute["type"], attribute["is-primary-key"]))
+
+        new_entity = Entity(entity["name"], attributes)
+        for relation in entity["relations"]:
+            exists = False
+            for existing_relation in relations:
+                if existing_relation.name == relation["name"]:
+                    existing_relation.set_right(new_entity, relation["cardinality"]) 
+                    exists = True
+                    break
+            if not exists:
+                new_relation = Relation(relation["name"])
+                new_relation.set_left(new_entity, relation["cardinality"])
+                relations.append(new_relation)
+
+        entities.append(new_entity)
+
+    return entities, relations
+            
+            
 
 
 # öffnet die Datei im Lese-Modus
